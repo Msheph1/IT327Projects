fcddf678ecc3620ab76b8b5b2456a7a683d5554c
Msheph1 <max.r.shep@gmail.com>
2023-12-03 22:03:02 -0600

    core methods done pretty much

--- Advanced/Program.cs
+++ Advanced/Program.cs
@@ -27,8 +27,61 @@ public class SongNode{
 public class Playlist{
     private List<Song> Songs {get; set;}
     private SongNode DummyHead;
-    private SongNode DummyTail;
-    
+    private SongNode Tail;
+    public Playlist()
+    {
+        Songs = new List<>();
+        Head = new SongNode();
+        Tail = new SongNode();
+        Head.Next = Tail
+    }
+
+    public void AddSong(Song song){
+        SongNode temp = new SongNode(song);
+        if(Tail.SongData == null)
+        {
+            Tail = temp;
+        }
+        else{
+            Tail.Next = temp;
+            Tail = temp;
+        }
+        
+    }
+
+    public void PlayNext(Song song)
+    {
+        SongNode temp = DummyHead.Next;
+        DummyHead.Next = new SongNode(song);
+        DummyHead.Next.Next = temp;
+    }
+
+    public void RemoveSong(string name, string author)
+    {
+        SongNode iter = DummyHead;
+        SongNode lastIter = null;
+        while(iter.Next != null)
+        {
+            if(iter.Next.SongData.SongName.get().equals(name) && iter.Next.SongData.SongAuthor.get().equals(autho))
+            {
+                iter.Next = iter.Next.Next;
+            }
+            if(iter.Next == null)
+            {
+                lastIter = iter;
+            }
+            iter = iter.next;
+        }
+        if(lastIter != null)
+        {
+            lastIter.Next = null;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "";
+    }
 
 }
 
