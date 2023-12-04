82a106f48d98decf708645c1fc669f635f1c4048
mhwilso <mhwilso@ilstu.edu>
2023-12-04 11:16:32 -0600

    Added Fibonacci

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 10; // Change this value to generate more Fibonacci numbers
+        Console.WriteLine($"First {n} Fibonacci numbers:");
+
+        foreach (int fibNumber in GenerateFibonacci(n))
+        {
+            Console.Write($"{fibNumber} ");
+        }
+
+        Console.ReadLine();
+    }
+
+    static IEnumerable<int> GenerateFibonacci(int n)
+    {
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            yield return a;
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+}
