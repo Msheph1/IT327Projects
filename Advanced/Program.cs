65f9639c40d59b95f3587fb7eb1873d96d6cd0d8
Msheph1 <max.r.shep@gmail.com>
2023-12-03 21:16:23 -0600

    song and song node done

--- Advanced/Program.cs
+++ Advanced/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Advanced program that implements a music playlist using a linkedlist
+//By Max Shepherd
+
+using System;
+public class Song{
+    private string SongName {get; set;}
+    private string SongAuthor {get; set;}
+    public Person(string songName, songAuthor)
+    {
+        SongName = songName;
+        SongAuthor = songAuthor;
+    }
+
+}
+
+public class SongNode{
+    public Song SongData;
+    public SongNodeNext;
+    public SongNode(Song song = null)
+    {
+        SongData = song;
+        Next = null;
+    }
+
+}
+
+public class Playlist{
+    private List<Song> Songs {get; set;}
+    private SongNode DummyHead;
+    private SongNode DummyTail;
+    
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to your music playlist!\nThe current playlist is " + "playlist.toString()" +"\n\nWhat do you want to do\n1 - Add a song\n2 - Play next\n3 - Remove a song\n4 - Exit");
+    }
+}
--- Advanced/ReadMe.txt
+++ Advanced/ReadMe.txt
@@ -1,1 +1,2 @@
-Advanced Project
+Advanced Project -
+This is a music playlist manager that holds songs and allows the user to  remove, playnext, and add a song.
