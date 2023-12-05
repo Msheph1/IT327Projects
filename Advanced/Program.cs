b3e52628986d5ab5d9a549a6068218b61da8f95e
Msheph1 <max.r.shep@gmail.com>
2023-12-05 11:48:17 -0600

    almost done

--- Advanced/Program.cs
+++ Advanced/Program.cs
@@ -3,20 +3,25 @@
 
 using System;
 public class Song{
-    private string SongName {get; set;}
-    private string SongAuthor {get; set;}
-    public Person(string songName, songAuthor)
+    public string SongName {get; set;}
+    public string SongAuthor {get; set;}
+    public Song(string songName, string songAuthor)
     {
         SongName = songName;
         SongAuthor = songAuthor;
+    }
+
+    public override string ToString()
+    {
+        return SongName + " By " + SongAuthor;
     }
 
 }
 
 public class SongNode{
     public Song SongData;
-    public SongNodeNext;
-    public SongNode(Song song = null)
+    public SongNode Next;
+    public SongNode(Song song)
     {
         SongData = song;
         Next = null;
@@ -25,62 +30,86 @@ public class SongNode{
 }
 
 public class Playlist{
-    private List<Song> Songs {get; set;}
+    public List<Song> Songs {get; set;}
     private SongNode DummyHead;
     private SongNode Tail;
     public Playlist()
     {
-        Songs = new List<>();
-        Head = new SongNode();
-        Tail = new SongNode();
-        Head.Next = Tail
+        Songs = new List<Song>();
+        Song NullSong = new Song("","");
+        DummyHead = new SongNode(NullSong);
+        Tail = new SongNode(NullSong);
+        DummyHead.Next = Tail;
     }
 
     public void AddSong(Song song){
-        SongNode temp = new SongNode(song);
-        if(Tail.SongData == null)
+        SongNode Temp = new SongNode(song);
+        if(Tail.SongData.SongName == null)
         {
-            Tail = temp;
+            Tail = Temp;
+            Console.WriteLine("done");
         }
         else{
-            Tail.Next = temp;
-            Tail = temp;
+            Tail.Next = Temp;
+            Tail = Temp;
         }
+
         
     }
 
-    public void PlayNext(Song song)
+    public void PlayNext()
     {
-        SongNode temp = DummyHead.Next;
-        DummyHead.Next = new SongNode(song);
-        DummyHead.Next.Next = temp;
+        
+        if(DummyHead.Next.Next != null)
+        {
+        DummyHead.Next = DummyHead.Next.Next;
+        DummyHead = DummyHead.Next;
+        }
+        else{
+            Tail = new SongNode(new Song("",""));
+            DummyHead.Next = Tail;
+        }
     }
 
     public void RemoveSong(string name, string author)
     {
-        SongNode iter = DummyHead;
-        SongNode lastIter = null;
-        while(iter.Next != null)
+        SongNode? Iter = DummyHead;
+        SongNode? LastIter = null;
+        while(Iter?.Next != null)
         {
-            if(iter.Next.SongData.SongName.get().equals(name) && iter.Next.SongData.SongAuthor.get().equals(autho))
+             
+            if(Iter.Next.SongData.SongName.Equals(name) && Iter.Next.SongData.SongAuthor.Equals(author))
             {
-                iter.Next = iter.Next.Next;
+                Iter.Next = Iter.Next.Next;
             }
-            if(iter.Next == null)
+            if(Iter.Next == null)
             {
-                lastIter = iter;
+                LastIter = Iter;
             }
-            iter = iter.next;
+            Iter = Iter.Next;
         }
-        if(lastIter != null)
+        if(LastIter != null)
         {
-            lastIter.Next = null;
+            LastIter.Next = null;
         }
     }
 
+
     public override string ToString()
     {
-        return "";
+        string Ret = "\n";
+        if(DummyHead.Next == Tail)
+        {
+            return "Empty!\n";
+        }
+        SongNode? Iter = DummyHead.Next;
+        Ret += "Playing Now - ";
+        while(Iter != null)
+        {
+            Ret += Iter.SongData.SongName + "By " + Iter.SongData.SongAuthor + "\n";
+            Iter = Iter.Next;
+        }
+        return Ret + "\n\n";
     }
 
 }
@@ -89,6 +118,92 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to your music playlist!\nThe current playlist is " + "playlist.toString()" +"\n\nWhat do you want to do\n1 - Add a song\n2 - Play next\n3 - Remove a song\n4 - Exit");
+
+        Playlist pl = new Playlist();
+        List<Song> StartingSongs = new List<Song>();
+
+        StartingSongs.Add(new Song("Cruel Summer","Taylor Swift"));
+        StartingSongs.Add(new Song("Paint the Town Red", "Doja Cat"));
+        StartingSongs.Add(new Song("All I Want For Christmas Is You","Mariah Carey"));
+        StartingSongs.Add(new Song("Snooze","SZA"));
+        StartingSongs.Add(new Song("I Remember Everything","Zack Bryan"));
+        StartingSongs.Add(new Song("Greedy","Tate McRae"));
+        StartingSongs.Add(new Song("Rockin' Around The Christmas Tree","Brenda Lee"));
+        StartingSongs.Add(new Song("Is It Over Now?","Taylor Swift"));
+        StartingSongs.Add(new Song("Water","Tyla"));
+        pl.Songs = StartingSongs;
+        pl.AddSong(new Song("You Broke My Heart", "Drake"));
+        pl.AddSong(new Song("Jingle Bell Rock", "Bobby Helms"));
+        pl.AddSong(new Song("Cruel Summer","Taylor Swift"));
+
+        string? input = "-1";
+        while(!input.Equals("4"))
+        {
+            Console.WriteLine("Welcome to your music playlist!\nThe current playlist is: " + pl.ToString() +"What do you want to do\n1 - Add a song\n2 - Play next\n3 - Remove a song\n4 - Exit");
+            input = Console.ReadLine();
+            if(input != null){
+                
+            //Add song
+            if(input.Equals("1"))
+            {   
+                List<Song> SongList = pl.Songs;
+                Console.WriteLine("The Song List contains: ");
+                int Count = 1;
+                foreach(Song Song in SongList)
+                    {
+                        Console.WriteLine(Count + "- " + Song.SongName + " By " + Song.SongAuthor);
+                        Count++;
+                    }
+                Console.WriteLine(Count + "- Add none of these");
+                int SongInput = -1;
+                while(SongInput < 0 || SongInput > SongList.Count())
+                {
+                    string? UserIn = Console.ReadLine();
+                    if(int.TryParse(UserIn, out SongInput))
+                    {
+                    
+                    if(SongInput < 0 || SongInput > SongList.Count())
+                        {
+                            Song temp = SongList[SongInput -1];
+                            Console.WriteLine(temp);
+                            pl.AddSong(temp);
+                        }
+                        else if(SongInput == SongList.Count + 1)
+                        {
+                            SongInput = 1;
+                            //break out
+                        }
+                        else{
+                            Console.WriteLine("Invalid Input Please Try Again.");
+                        }
+                    }
+                    else{
+                        Console.WriteLine("Invalid Input Please Try Again.");
+                    }
+                }
+                    
+            }
+            //Play Next
+            else if(input.Equals("2"))
+            {
+                pl.PlayNext();
+            }
+            //Remove Song
+            else if(input.Equals("3"))
+            {
+            
+            }
+            else if(input.Equals("4"))
+            {
+                ;
+            }
+            else{
+                Console.WriteLine("Invalid Input Please Try Again");
+            }
+            }
+            else{
+                input = "-1";
+            }
+        }
     }
 }
